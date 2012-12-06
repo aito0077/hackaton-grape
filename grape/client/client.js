b5e83db37c4cc93e0180d505b2ff4712b80f745c
Hernando Ariel Farias <bubastix0@gmail.com>
2012-12-06 04:08:54 -0300

    ahora se puede ver el perfil (sin datos) reconoce si la iniciativa fue creada por el que la ve

--- grape/client/client.js
+++ grape/client/client.js
@@ -133,9 +133,41 @@ Template.sesumaron.participantes = function(){
   return Participantes.find({iniciativa:Session.get('iniciativa')});
 }
 
+Template.iniciativa.soyCreador = function () {
+  var creador = Meteor.users.findOne(this.creador);
+  return creador._id == Meteor.userId();
+};
+
 Template.iniciativa.detail = function () {
   return Iniciativas.findOne(Session.get("iniciativa"));
 };
+
+Template.iniciativa.events({
+  'click .ver-perfil':function(event){
+    Session.set('page','perfil');
+    Session.set('perfil',$(event.currentTarget).attr('data-perfil'));
+    console.log($(event.currentTarget).attr('data-perfil'));
+
+  }
+});
+
+Template.sesumaron.events({
+  'click .ver-perfil':function(event){
+    Session.set('page','perfil');
+    Session.set('perfil',$(event.currentTarget).attr('data-perfil'));
+    console.log($(event.currentTarget).attr('data-perfil'));
+
+  }
+});
+
+Template.ItemListadoInicio.events({
+  'click .ver-perfil':function(event){
+    Session.set('page','perfil');
+    Session.set('perfil',$(event.currentTarget).attr('data-perfil'));
+    console.log($(event.currentTarget).attr('data-perfil'));
+
+  }
+});
 
 Template.sidebarDatos.events({
   'click .participar': function(){
